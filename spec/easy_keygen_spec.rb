e1e8ac7caa7cfa0677338a44781478cfbaef36e8
john <john@pixelhipsters.com>
2013-09-26 06:43:12 +0000

    Update easy_keygen spec with more examples

--- spec/easy_keygen_spec.rb
+++ spec/easy_keygen_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
 describe EasyKeygen do
+  let(:easy_keygen) { EasyKeygen }
   it 'returns have a version number' do
     EasyKeygen::VERSION.should_not be_nil
   end
 
-  it 'returns an error' do
-    false.should be_true
+  it 'returns false' do
+    false.should be_false
+  end
+  
+  describe '#size_of_word' do
+    context 'when no arguments are provided' do
+      it 'returns a default of 4' do
+        expect(easy_keygen.size_of_word).to eq(4)
+      end
+    end
+    
+    context 'when called with argument' do
+      it 'returns that number' do
+        expect(easy_keygen.size_of_word(30)).to eq(30)
+      end
+    end
   end
 end
