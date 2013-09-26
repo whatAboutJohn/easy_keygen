55264c915009a4f5570567c7aec779402c02b0d7
john <john.molinam@hotmail.com>
2013-09-25 20:33:28 -0400

    Worked on parsing options

--- lib/easy_key_gen.rb
+++ lib/easy_key_gen.rb
@@ -11,10 +11,6 @@ random_key = (0...36).map{|i|i.to_s 36}
 
 generate_random_key(random_key)
 
-
-require 'optparse'
-require 'ostruct'
-
 ##############################################################
 
 
@@ -25,35 +21,48 @@ module EasyKeyGen
 
 	extend self
 
-	ConfigStruct = Struct.new (
-		:size,
-		:amount,
-		:letters,
-		:encryption
-	)
+	# ConfigStruct = Struct.new {
+	# 	:size => 4,
+	# 	:amount => 4,
+	# 	:include => 'both',
+	# 	:encryption
+	# }
 	
 	def size_of_word
 
 	end
 
-	options = {}
+	options = {
+	 	:size => 4,
+	 	:amount => 4,
+	 	:_include => 'both',
+	 	:encryption
+	}
 
 	optparse = OptionParser.new do |opts|
 
-		opts.on '-h', '--help', '/?' do
-			# Help screen
+		opts.banner = 'Usage: easy_key_gen.rb [options]'
+
+		opts.on '-s', '--size size', 'Amount of word groups to generate.' do |size|
+			options[:size] = size
 		end
 
-		opts.on ''
+		opts.on '-a', '--amount amount', 'Amount of letters per word group.' do |amount|
+			options[:amount] = amount
+		end
+
+		opts.on '-i', '--include include', 'Specify to generate letters and / or numbers.' do |_include|
+			options[:_include] = _include
+		end
+
+		opts.on '-h', '--help', 'Show help information.' do
+			puts opts
+			exit
+		end
 
 	end
 
+	# Parse this mutha-fuckaaaa!
+	optparse.parse!
+
 end
-
-__END__
-
-Options include:
-  - Size of words	-s --size
-  - Amount of letters -a --amount
-  - Letters and/or numbers -i --include
-
